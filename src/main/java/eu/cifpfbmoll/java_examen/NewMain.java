b6c445b11b6ad92f68101ea4ab67002dde50aab3
Marat-Rafael <mazizov@cifpfbmoll.eu>
2021-03-03 19:05:53 +0100

    añadido redBibliotecas

---
+++ src/main/java/eu/cifpfbmoll/java_examen/NewMain.java
@@ -0,0 +1,17 @@
+
+package eu.cifpfbmoll.java_examen;
+
+/**
+ *
+ * @author Marat Rafael
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
